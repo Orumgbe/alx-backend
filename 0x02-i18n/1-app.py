c42e49dfb202c53e4bff52aa57c50bef28708289
Orumgbe <orumgbeugbekile@gmail.com>
2023-10-31 10:21:08 -0700

    internationalisation

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module has basic setup for a flask app"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """Configure available languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Returns the locale of user"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """Render template for index page"""
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
