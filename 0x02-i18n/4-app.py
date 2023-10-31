d345f2c0ca1c239341776674f80e0e472b33b061
Orumgbe <orumgbeugbekile@gmail.com>
2023-10-31 16:15:11 -0700

    query param coded locale

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,14 +20,17 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """Returns the locale of user"""
+    """Returns the best match locale of user"""
+    locale = request.args.get('locale')
+    if locale is not None:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/", strict_slashes=False)
 def index():
     """Render template for index page"""
-    return render_template("1-index.html")
+    return render_template("4-index.html")
 
 
 if __name__ == "__main__":
