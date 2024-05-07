35f163f410d6ee529a62d3ba1cdffa157285d56e
Orumgbe <orumgbeugbekile@gmail.com>
2024-05-07 02:03:34 -0700

    Staging all changes and requirement files

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module has class BasicCache"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """Basic caching system"""
+    def __init__(self):
+        super.__init__(self)
