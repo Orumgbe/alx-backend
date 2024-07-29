15fc209f1844871efd30322c4accec7579a7fb89
Orumgbe <orumgbeugbekile@gmail.com>
2024-07-29 14:59:39 +0100

    hypermedia pagination

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+"""Deletion-resilient hypermedia pagination
+"""
+import csv
+import math
+from typing import Dict, List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initializes a new Server instance.
+        """
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Return page info based on page_index and page_size
+        """
+        data = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(data.keys())
+        data = []
+        count = 0
+        next = None
+        start = index if index else 0
+        for i, item in data.items():
+            if i >= start and count < page_size:
+                data.append(item)
+                count += 1
+                continue
+            if count == page_size:
+                next = i
+                break
+        page_info = {
+            'index': index,
+            'next_index': next,
+            'page_size': len(data),
+            'data': data,
+        }
+        return page_info
